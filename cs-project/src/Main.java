1265fc46d851dcf97223c4de2abb30b4df593f76
WenzhiMichael <91454276+WenzhiMichael@users.noreply.github.com>
2023-04-18 20:34:32 +0700

    demo

--- cs-project/src/Main.java
+++ cs-project/src/Main.java
@@ -1,62 +1,94 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import java.io.BufferedReader;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-
 public class Main {
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Top Champions Table");
+        SwingUtilities.invokeLater(() -> {
+            try {
+                createAndShowGUI();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private static void createAndShowGUI() throws IOException {
+        JFrame frame = new JFrame("LoL Champions");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
 
-        DefaultTableModel model = new DefaultTableModel(new String[]{"Rank", "Champion", "Tier", "Win Rate", "Pick Rate", "Ban Rate", "Weak Against"}, 0);
+        JPanel topPanel = new JPanel();
+        JButton topButton = new JButton("Top");
+        JButton jungleButton = new JButton("Jungle");
+        JButton midButton = new JButton("Mid");
+        JButton adcButton = new JButton("ADC");
+        JButton supportButton = new JButton("Support");
 
-        // Read the data from the CSV file and add it to the table model
-        String csvFile = "top.csv";
-        String line;
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-            // Skip the first line
-            br.readLine();
+        topPanel.add(topButton);
+        topPanel.add(jungleButton);
+        topPanel.add(midButton);
+        topPanel.add(adcButton);
+        topPanel.add(supportButton);
 
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                model.addRow(values);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try (BufferedReader br = new BufferedReader(new FileReader("top.csv"))) {
-            while ((line = br.readLine()) != null) {
-                // Process the line...
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        JTable table = new JTable(model);
-
-        // Set the row height to a larger value
-        table.setRowHeight(40);
-
-        // Set the color of the grid lines to black
-        table.setGridColor(Color.BLACK);
-
-        // Show horizontal and vertical grid lines
-        table.setShowGrid(true);
-        table.setShowHorizontalLines(true);
-        table.setShowVerticalLines(true);
-
-        // Set the font of the table cells to be bigger
-        Font font = new Font("Arial", Font.PLAIN, 18);
-        table.setFont(font);
+        JTable table = new JTable();
+        table.setFillsViewportHeight(true);
 
         JScrollPane scrollPane = new JScrollPane(table);
-        frame.add(scrollPane);
+        frame.getContentPane().add(topPanel, BorderLayout.NORTH);
+        frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
 
-        frame.setSize(800, 600);
+        ActionListener buttonListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String csvFile;
+                if (e.getSource() == topButton) {
+                    csvFile = "top.csv";
+                } else if (e.getSource() == jungleButton) {
+                    csvFile = "jungle.csv";
+                } else if (e.getSource() == midButton) {
+                    csvFile = "mid.csv";
+                } else if (e.getSource() == adcButton) {
+                    csvFile = "adc.csv";
+                } else {
+                    csvFile = "support.csv";
+                }
+                updateTable(csvFile, table);
+            }
+        };
+
+        topButton.addActionListener(buttonListener);
+        jungleButton.addActionListener(buttonListener);
+        midButton.addActionListener(buttonListener);
+        adcButton.addActionListener(buttonListener);
+        supportButton.addActionListener(buttonListener);
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+
+    private static void updateTable(String csvFile, JTable table) {
+        String[] columnNames = {"Rank", "Champion", "Tier", "Win Rate", "Pick Rate", "Ban Rate", "Weak Against"};
+        DefaultTableModel tableModel = new DefaultTableModel(columnNames, 0);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            String line;
+            br.readLine(); // Skip the first line
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                tableModel.addRow(values);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        table.setModel(tableModel);
     }
 }
