d02d46eb5af549dd7f7607092529b27b631d1a64
WenzhiMichael <91454276+WenzhiMichael@users.noreply.github.com>
2023-04-18 19:52:07 +0700

    CS project 2

--- cs-project/src/Main.java
+++ cs-project/src/Main.java
@@ -1,33 +1,60 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.io.BufferedReader;
+import java.awt.*;
 import java.io.FileReader;
 import java.io.IOException;
 
+
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Champions Table");
+        JFrame frame = new JFrame("Top Champions Table");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         DefaultTableModel model = new DefaultTableModel(new String[]{"Rank", "Champion", "Tier", "Win Rate", "Pick Rate", "Ban Rate", "Weak Against"}, 0);
 
-        try (BufferedReader br = new BufferedReader(new FileReader("top.csv"))) {
-            String line;
+        // Read the data from the CSV file and add it to the table model
+        String csvFile = "top.csv";
+        String line;
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            // Skip the first line
+            br.readLine();
+
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(",");
                 model.addRow(values);
-                System.out.println("Added row: " + String.join(", ", values));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader("top.csv"))) {
+            while ((line = br.readLine()) != null) {
+                // Process the line...
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         JTable table = new JTable(model);
+
+        // Set the row height to a larger value
+        table.setRowHeight(40);
+
+        // Set the color of the grid lines to black
+        table.setGridColor(Color.BLACK);
+
+        // Show horizontal and vertical grid lines
+        table.setShowGrid(true);
+        table.setShowHorizontalLines(true);
+        table.setShowVerticalLines(true);
+
+        // Set the font of the table cells to be bigger
+        Font font = new Font("Arial", Font.PLAIN, 18);
+        table.setFont(font);
+
         JScrollPane scrollPane = new JScrollPane(table);
         frame.add(scrollPane);
-
-        System.out.println("Table rows: " + model.getRowCount());
-        System.out.println("Table columns: " + model.getColumnCount());
 
         frame.setSize(800, 600);
         frame.setVisible(true);
